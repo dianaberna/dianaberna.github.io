15199022ff9429075dd33c34c7f143990bdd98a5
Diana <bernabei.d@gmail.com>
2021-11-09 00:06:55 +0100

    upd: about page

--- pages/about.vue
+++ pages/about.vue
@@ -24,7 +24,7 @@
         <h3 class="text-xl md:text2xl lg:text-3xl text-primary text-gray-700">Esperienze lavorative</h3>
         <ul class="list-disc">
           <li>( 2021 - in corso ) <b>TomorrowDevs</b> - Community Manager</li>
-          <li>( 2020 - in corso ) <b>Freelance</b> - Web developer, UI designer (Wordpress, JAMstack)</li>
+          <li>( 2020 - in corso ) <b>Freelance</b> - Web developer, UI designer  (Collaborazioni con: Kepler Informatica, Moncler, MadProduction)</li>
           <li>( 2020 ) <b>Zerynth</b> sede Pisa (PI) - Technical Marketing, Content marketing, Copywriting</li>
           <li>( 2018 - 2019 ) <b>Libraccio</b> sede Pisa - Commessa</li>
           <li>( 2012 - 2018 ) <b>Ecopoint S.r.l.</b> sede Fermo (FM) - Sviluppo sito web vetrina, Sviluppo e-commerce da analisi di mercato dei competitor del settore VMC e mancanza di negozi online di filtri per tali macchine, Ottimizzazione SEO, Grafiche per volantini, carta intestata, blocchi appunti, bigliettini da visita</li>
@@ -33,6 +33,7 @@
       <div class="text-base md:text-lg font-regular text-gray-700 mt-2">
         <h3 class="text-xl md:text2xl lg:text-3xl text-primary text-gray-700"><span class="under community">Esperienze di community</span></h3>
         <ul class="list-disc">
+          <li>( 2021 ) MC Devcast Serie Frontend di Codemotion <a href="https://live.codemotion.com/devcast/la-compagnia-del-frontend">"La compagnia del frontend"</a></li>
           <li>( 2021 ) <b>dianacodes</b> - Community Manager</li>
           <li>( 2017 - ora ) <a href="https://www.womentechmakers.com/" target="_blank" class="font-bold"><b>WomenTechmakers</b></a> - Ambassador</li>
           <li>( 2016 - 2020 ) <a href="https://gdgpisa.it/" target="_blank" class="font-bold"><b>GDG Pisa</b></a> - Community Manager</li>
@@ -44,7 +45,7 @@
         <h3 class="text-xl md:text2xl lg:text-3xl text-primary text-gray-700"><span class="under hard">Educazione</span></h3>
         <ul class="list-disc">
           <li>( 2021 ) Laurea triennale in <span class="font-bold">INFORMATICA</span> presso l'<span class="font-bold">Università di Camerino</span> </li>
-          <li>( 2018 - 2021 ) Laurea triennale in <span class="font-bold">INFORMATICA</span> (non conclusa) presso l'<span class="font-bold">Università di Pisa</span> (Esami: Ricerca operativa, Architettura degli elaboratori, Logica per la programmazione, Progettazione grafica, Algortimica, Progettazione d'interfaccie, Sistemi operativi)</li>
+          <li>( 2018 - 2021 ) Laurea triennale in <span class="font-bold">INFORMATICA</span> (non conclusa) presso l'<span class="font-bold">Università di Pisa</span> (Esami: Ricerca operativa, Architettura degli elaboratori, Logica per la programmazione, Progettazione grafica, Algortimica, Progettazione di interfacce, Sistemi operativi)</li>
           <li>( 2013 - 2018 ) Laurea triennale in <span class="font-bold">INGEGNERIA INFORMATICA</span> (non conclusa) presso l'<span class="font-bold">Università di Pisa</span> (Esami: Analisi 1, Analisi 2, Algebra, Calcolo Numerico, Fisica, Fondamenti di Informatica 1, Progettazione Web, Ingegneria del software, Elettrotecnica, Basi di dati, Fondamenti di automatica)</li>
           <li>( 2009 - 2013 ) Diploma scuola superiore specializzazione <span class="font-bold">INFORMATICA</span> presso <span class="font-bold">ITIS “G. e M. Montani”</span> di Fermo - Voto 100/100 - Linguaggi: Assembly, C, COBOL, Javascript, PASCAL, PHP, SQL</li>
         </ul>
@@ -65,7 +66,19 @@
       </div>
       <div class="text-base md:text-lg font-regular text-gray-700 mt-2">
         <h3 class="text-xl md:text2xl lg:text-3xl text-primary text-gray-700">Skills</h3>
-        <img src="~/assets/skills.png" class="" alt="grafico skills"><br>
+        <p>Da quando mi sono avvicinata alla programmazione con FORTRAN all'età di 13 anni ho sempre voluto sperimentare nuovi linguaggi e tecnologie.</p><br>
+        <p>Con il corso di informatica dell'ITI ho avuto modo di vedere <span class="under hard">C/C++, PHP, COBOL, PASCAL, SQL, Assembly e HTML/CSS</span>. E' da questo momento che comprendo 
+          la passione per il design e il frontend, infatti mi avvicino alla grafica con <span class="under design">Photoshop/Illustrator e al web design</span>. 
+        </p><br>
+        <p>Nel percorso universitario ho visto <span class="under hard">C/C++, Javascript, Assembly</span>.</p>
+        <p>Sempre negli anni dell'università ho approfondito la <span class="under design">teoria UI/UX</span>, ho studiato i preprocessori <span class="under hard">SASS/SCSS, Javascript ES6 e il JAMStack</span>. 
+        <br>Oltre alle hard skills  ho iniziato a coltivare in modo attivo le mie soft skills, infatti grazie alla <span class="under community">gestione della community</span> GDG Pisa ho imparato cosa vuol 
+        dire gestire un team e rispettare le scadenza prestabilite in "fase di progettazione".
+          In questo momento ho anche iniziato a migliorare le mio doti di <span class="under soft">public speaking</span>, e tutt'ora ci sto lavorando, portando talk a diversi eventi in giro per l'Italia.
+          Dal 2021 ho imparato ha parlare correttamente davanti ad uno schermo senza sentirmi in imbarazzo e a saper <span class="under community">gestire eventi in streaming</span>. 
+        </p>
+        
+        <!--<img src="~/assets/skills.png" class="" alt="grafico skills"><br>-->
       </div>
   </div>
 </template>
