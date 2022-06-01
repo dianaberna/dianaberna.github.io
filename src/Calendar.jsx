ec2cf9b679dd6f0f4113ad7d75517f66f2022074
dianaberna <bernabei.d@gmail.com>
2022-06-01 16:02:08 +0200

    feat: updated calendar

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -10,14 +10,12 @@ const Calendar = () => {
           <div className="border-solid border-2 border-black w-[5px] h-[20px] bg-black -mt-6 absolute right-[20px]">
             {' '}
           </div>
-          <div className="text-2xl font-bold">19 Maggio</div>
-          <div className="text-lg">Meetup MiaPlatform Online</div>
-          <div className="font-light my-2">
-            Accessibilità e tecnologia, da che parte cominciare?
-          </div>
+          <div className="text-2xl font-bold">2-5 Giugno</div>
+          <div className="text-lg">PYCON Italia 2022</div>
+          <div className="font-light my-2">Talk: Web Accessibility in mind</div>
           <a
             className="underline"
-            href="https://www.meetup.com/mia-platform-cultura-innovazione-team/events/285718193/"
+            href="https://pycon.it/en/talk/web-accessibility-in-mind?day=2022-06-04"
             target="_blank"
             rel="noreferrer"
           >
@@ -31,37 +29,14 @@ const Calendar = () => {
           <div className="border-solid border-2 border-black w-[5px] h-[20px] bg-black -mt-6 absolute right-[20px]">
             {' '}
           </div>
-          <div className="text-2xl font-bold">20-21 Maggio</div>
-          <div className="text-lg">Accessibility Days Milano + Online</div>
+          <div className="text-2xl font-bold">16-18 Giugno</div>
+          <div className="text-lg">Web Marketing Festival</div>
           <div className="font-light my-2">
-            Talk: Accessible React, Track 2, 15:30 - 16:15
+            Talk: Design system accessibile in React
           </div>
           <a
             className="underline"
-            href="https://accessibilitydays.it/2022/it/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Link per iscriversi all'evento
-          </a>
-        </div>
-      </div>
-      <div className="flex flex-col lg:flex-row gap-10">
-        <div className="flex flex-col border-solid border-2 border-black p-4 w-3/5 relative mt-10">
-          <div className="border-solid border-2 border-black w-[5px] h-[20px] bg-black -mt-6 absolute ml-[10px]">
-            {' '}
-          </div>
-          <div className="border-solid border-2 border-black w-[5px] h-[20px] bg-black -mt-6 absolute right-[20px]">
-            {' '}
-          </div>
-          <div className="text-2xl font-bold">24-25 Maggio</div>
-          <div className="text-lg">Workshop Fest Codemotion</div>
-          <div className="font-light my-2">
-            C’era una volta un componente #Storybook
-          </div>
-          <a
-            className="underline"
-            href="https://www.meetup.com/mia-platform-cultura-innovazione-team/events/285718193/"
+            href="https://www.webmarketingfestival.it/"
             target="_blank"
             rel="noreferrer"
           >
